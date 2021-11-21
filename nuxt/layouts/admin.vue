3fb5ba100fea1708eb20ced98fbe80527a3bfd23
Kiyo510 <kanzenziai_r@yahoo.co.jp>
2021-11-21 11:50:40 +0900

    Add:ログアウト機能を追加

--- nuxt/layouts/admin.vue
+++ nuxt/layouts/admin.vue
@@ -6,6 +6,11 @@
     <v-app-bar app>
       <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
       <v-toolbar-title>管理画面</v-toolbar-title>
+      <v-row justify="end">
+        <v-btn class="red white--text" @click="logout" type="submit"
+          >ログアウト</v-btn
+        >
+      </v-row>
     </v-app-bar>
     <v-main>
       <Nuxt />
@@ -18,11 +23,18 @@ import AdminSidebar from "@/components/admin/Sidebar.vue";
 
 export default {
   middleware: "not_logined_admin_user",
+  name: "admin",
   components: {
     AdminSidebar,
   },
   data: () => ({
     drawer: true,
   }),
+  methods: {
+    async logout() {
+      await this.$store.dispatch("auth/logout");
+      this.$router.push("/admin/login");
+    },
+  },
 };
 </script>
