63b78bb14ae7687744446d01ad5bde456b8aa3f4
Kiyo510 <kanzenziai_r@yahoo.co.jp>
2021-11-23 00:16:32 +0900

    Add:バリデーションの共通処理を実装

--- nuxt/nuxt.config.js
+++ nuxt/nuxt.config.js
@@ -20,7 +20,8 @@ export default {
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
     "~/plugins/axios",
-    '@plugins/vee-validate'
+    '@plugins/vee-validate',
+    '@plugins/mixins/validation.js'
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
--- nuxt/plugins/axios.js
+++ nuxt/plugins/axios.js
@@ -1,3 +1,14 @@
-export default function({ $axios }) {
+export default function({ $axios, store}) {
   $axios.defaults.headers.common["X-Requested-With"] = "XMLHttpRequest";
+
+  $axios.onError((error) => {
+    if (error.response.status === 422) {
+      store.dispatch('validation/setErrors', error.response.data.error.message)
+    }
+    return Promise.reject(error)
+  })
+
+  $axios.onRequest(() => {
+    store.dispatch('validation/clearErrors')
+  })
 }
